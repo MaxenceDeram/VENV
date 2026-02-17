ee013f1a551bb4f8fa9001cf44a8fc5c7b64a28e
maxderam <maxence.derm@efrei.net>
2026-02-17 10:32:49 +0100

    final

--- simple_api.py
+++ simple_api.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -8,7 +9,25 @@ def hello():
 
 @app.route('/info', methods=['GET'])
 def info():
-    return jsonify({ 'jaime trop les filles': 'je suis un homme'    })
+    return jsonify({'jaime trop les filles': 'je suis un homme'})
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    conn = mysql.connector.connect(
+        host="mysql-maxderam.alwaysdata.net",
+        user="maxderam",
+        password="waren59890",
+        database="maxderam_projectclothingv1"
+    )
+
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM catalog")
+    products = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return jsonify(products)
 
 if __name__ == "__main__":
     app.run(debug=True)
